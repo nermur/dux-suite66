1686ae587864403a78ae55c1cc4b4a21a7016c27
nermur <29991266+nermur@users.noreply.github.com>
2022-05-30 18:51:01 -0700

    test SCRIPT_DIR change

--- scripts/dux_installer.sh
+++ scripts/dux_installer.sh
@@ -11,7 +11,7 @@ wait
 
 git config --global --add safe.directory "/home/artix/dux"
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 cd "${SCRIPT_DIR}" && GIT_DIR=$(git rev-parse --show-toplevel)
 source "${GIT_DIR}/scripts/GLOBAL_IMPORTS.sh"
 source "${GIT_DIR}/configs/settings.sh"
